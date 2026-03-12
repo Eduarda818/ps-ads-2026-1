6be06b3650f4ba318da4dd155bd49e542e731fa8
Eduarda818 <marveeduarda9@gmail.com>
2026-03-12 15:20:28 +0000

    ENTREGA DA ATIVIDADE 1

--- back-end/src/routes/customers.js
+++ back-end/src/routes/customers.js
@@ -5,8 +5,14 @@ const router = Router()
 // onde for POST, vai no contoller e cria
 //POST precisa ter corpo na requisição pois ele precisa mandar dados
 router.post('/', controller.create)
-
 router.get('/', controller.retrieveAll)
+router.get('/:id', controller.retrieveOne) // parametro na rota funciona como uma váriavel, começa com :
+// não podemos ter mias que um parâmetro por rota
+router.put('/:id', controller.update)
+
+// HTTP: tem dois verbos de atualização:
+//  PUT: Substituição integral de objeto
+// PATCH: substituição parcial
 
 
 export default router
