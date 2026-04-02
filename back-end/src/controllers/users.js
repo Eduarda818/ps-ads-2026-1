8dcbc0078e579d28edf34df1dfd72ba8cc2594ae
Eduarda818 <marveeduarda9@gmail.com>
2026-04-02 15:21:11 +0000

    (02/04) Implementação do hashing de senhas no back-end

--- back-end/src/controllers/users.js
+++ back-end/src/controllers/users.js
@@ -1,5 +1,14 @@
 import { prisma } from '../database/client.js'
 
+import argon2 from 'argon2';
+
+
+const ARGON2_CONFIG = {
+ type: argon2.argon2id,  // variante recomendada do algoritmo
+ memoryCost: 65536,      // 64 KB de memória máxima utilizada
+ timeCost: 3,            // número de iterações
+ parallelism: 4          // número de threads simultâneas
+}
 
 const controller = {}   // Objeto vazio
 
@@ -10,6 +19,13 @@ const controller = {}   // Objeto vazio
 // res ~> representa a resposta (response)
 controller.create = async function(req, res) {
  try {
+   // Caso exista o campo "password" em req.body, é
+   // necessário gerar o hash da senha antes de
+   // armazená-la no BD, usando o algoritmo argon2
+   if(req.body.password) {
+     req.body.password = await argon2.hash(req.body.password, ARGON2_CONFIG)
+   }
+
    // Para a inserção no BD, os dados são enviados
    // dentro de um objeto chamado "body" que vem
    // dentro da requisição ("req")
@@ -37,8 +53,9 @@ controller.retrieveAll = async function(req, res) {
  try {
    // Recupera todos os registros de clientes, ordenados pelo
    // campo "name", ascendente
-   const result = await prisma.users.findMany({
-    orderBy: [ { fullname: 'asc'} ]
+   const result = await prisma.user.findMany({
+     omit: { password: true },     // Omite o campo "password" do resultado
+     orderBy: [ { fullname: 'asc' } ]
    })
 
 
@@ -61,7 +78,8 @@ controller.retrieveOne = async function(req, res) {
  try {
    // Busca no banco de dados apenas o registro indicado
    // pelo parâmetro "id"
-   const result = await prisma.users.findUnique({
+   const result = await prisma.user.findUnique({
+     omit: { password: true },     // Omite o campo "password" do resultado
      where: { id: Number(req.params.id) }
    })
 
@@ -148,5 +166,34 @@ controller.delete = async function(req, res) {
 }
 
 
+controller.login = async function(req, res) {
+ try {
+   // Busca o usuário no BD por meio dos campos
+   // "username" ou "email"
+   const user = await prisma.user.findUnique({
+     where: {
+       OR: [
+         { username: req.body?.username },
+         { email: req.body?.email }
+       ]
+     }
+   })
+
+
+   // Se o usuário não for encontrado, retorna
+   // HTTP 401: Unauthorized
+   if(! user) {
+     console.error(`ERRO DE LOGIN: usuário "${req.body?.username}" ou e-mail "${req.body?.email}" não encontrado`)
+     return res.send(401).end()
+   }
+
+
+ }
+ catch(error) {
+   console.error(error)
+   // HTTP 500: Internal Server Error
+   res.status(500).end()
+ }
+}
 
 export default controller
